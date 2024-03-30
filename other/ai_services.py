6239e72319b108e30bfbc4438b29c75ed2bc8510
jeremy-jmc <jeremy.matos@utec.edu.pe>
2024-03-30 16:31:59 -0500

    ai APIs projects

---
+++ other/ai_services.py
@@ -0,0 +1,41 @@
+# https://youtu.be/5htSGLCroc8?si=FFS3LcR0Am26bvRn
+
+# https://elevenlabs.io/pricing
+# https://promptbase.com/marketplace?itemType=prompt&domain=text
+# https://www.jointaro.com/
+# https://neetcode.io/
+# https://www.together.ai/
+# https://stability.ai/
+# https://replicate.com/vaibhavs10/incredibly-fast-whisper
+# https://github.com/argmaxinc/WhisperKit
+# https://relevanceai.com/
+# https://huggingface.co/
+    # https://huggingface.co/tasks/token-classification
+
+# https://www.chatpdf.com/
+# https://www.dgl.ai/
+# https://domoai.app/
+
+"""
+SFT: Supervised Fine Tuning
+RLHF: Reinforcement Learning Human Feedback
+RAG: Retrieval Augmented Generation
+"""
+
+
+"""
+    https://youtu.be/sJBO7rMR8ks?si=fxOZRO888HAY8ZBD
+
+MATH: is just the formalization of the human idea
+MATH: practise, practise, practise
+
+
+Understanding math the right way is crucial for learning machine learning.
+Translate human ideas into math concepts
+Understanding math derivation rules simplifies ML learning
+Recognize patterns and practice is key to mastering ML.
+Realizing that debugging is coding helps with implementation
+Understanding large code bases with a simple strategy
+Setting breakpoints in the main function for debugging is crucial in understanding ML implementations
+Mastering ML takes time and dedication.
+"""
